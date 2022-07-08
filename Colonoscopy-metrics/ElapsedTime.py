1dcaf3a1d9a59426ff7963a8f883e36351e161a7
Keiran Barr <keiranbarr7@gmail.com>
2022-07-08 14:44:38 -0400

    Initialize repo

---
+++ Colonoscopy-metrics/ElapsedTime.py
@@ -0,0 +1,33 @@
+from PythonMetricsCalculator import PerkEvaluatorMetric
+
+class ElapsedTime( PerkEvaluatorMetric ):
+  
+  # Static methods
+  @staticmethod
+  def GetMetricName():
+    return "Elapsed Time"
+    
+  @staticmethod
+  def GetMetricUnit():
+    return "s"
+    
+  @staticmethod
+  def IsShared():
+    return True
+    
+  
+  # Instance methods
+  def __init__( self ):
+    PerkEvaluatorMetric.__init__( self )
+    
+    self.elapsedTime = 0
+    self.timePrev = None
+    
+  def AddTimestamp( self, time, matrix, point, role ):  
+    if ( self.timePrev != None ):
+      self.elapsedTime = self.elapsedTime + ( time - self.timePrev )
+      
+    self.timePrev = time
+    
+  def GetMetric( self ):
+    return self.elapsedTime
---
+++ Colonoscopy-metrics/PathLength.py
@@ -0,0 +1,39 @@
+import math
+from PythonMetricsCalculator import PerkEvaluatorMetric
+
+class PathLength( PerkEvaluatorMetric ):
+  
+  # Static methods
+  @staticmethod
+  def GetMetricName():
+    return "Path Length"
+
+  @staticmethod
+  def GetMetricUnit():
+    return "mm"
+    
+  @staticmethod
+  def IsPervasive():
+    return True
+    
+  @staticmethod
+  def IsShared():
+    return True
+    
+  
+  # Instance methods
+  def __init__( self ):
+    PerkEvaluatorMetric.__init__( self )
+    
+    self.pathLength = 0
+    self.pointPrev = None
+    
+  def AddTimestamp( self, time, matrix, point, role ):
+    if ( self.pointPrev != None ):
+      currPath = math.sqrt( math.pow( point[0] - self.pointPrev[0], 2 ) + math.pow( point[1] - self.pointPrev[1], 2 ) + math.pow( point[2] - self.pointPrev[2], 2 ) )
+      self.pathLength = self.pathLength + currPath
+
+    self.pointPrev = point[:] # Require element copy 
+       
+  def GetMetric( self ):
+    return self.pathLength
