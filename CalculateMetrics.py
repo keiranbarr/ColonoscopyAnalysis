bf1c9bd7890e8940f327d35b46d3634c3dc7cd35
Keiran Barr <keiranbarr7@gmail.com>
2022-07-11 14:17:39 -0400

    Implemented saving to file(s)

--- CalculateMetrics.py
+++ CalculateMetrics.py
@@ -1,37 +1,41 @@
 # This code is for testing the Perk Evaluator slicer module
 # By calculating metrics on a single transform sequence for a single metric
 
-# TODO: decide on data organization scheme
-# TODO: implement calibration transform hierarchy
+# TODO: Implement transform hierarchy
+    # Load calibration files
+    # Apply transforms
+# TODO: calculate on all patients in a directory
+# TODO: Fix memory leaks
 
-    # Identify patient ID
-    # Make folder for patient (if it doesn't already exist)
-    # Place ouptput spreadsheets in the folder
-
-# TODO: more robust pervasive-proofing
-# TODO: save results to excel
-# TODO: loop through specified patients
+slicer.mrmlScene.Clear()
 
 import os
 
+# User-defined
 analyzePracticeData = False
 saveResults = True
 
+# Make list of all possible practice data names
 formatter = "{:04d}".format
 practiceDataNameList = [formatter(i) for i in range(1000)]
 
-scenePath = r'P:\data\PerkTutor\Colonoscopy\2016-2017-Protocol-Reorganized\Data\Novices\PerkLab5\2017-04-29\26-PracticeSecondHalf-Posttest\2017-04-29-Scene.mrb'
+# User-defined
+patientPath = r'P:\data\PerkTutor\Colonoscopy\2016-2017-Protocol-Reorganized\Data\Novices\PerkLab5\2017-04-29\26-PracticeSecondHalf-Posttest'
 metricScriptPath = r'C:\Users\Keiran Barr\Documents\Summer 22\ColonoscopyAnalysis\Colonoscopy-metrics'
 outputSavePath = r'P:\data\PerkTutor\Colonoscopy\2016-2017-Protocol-Reorganized\Analysis\PerkTutorResults2022'
+
+patientID = os.path.basename(patientPath)
 
 # Find path of all metric scripts
 metricScriptPathList = []
 for file in os.listdir(metricScriptPath):
     filepath = os.path.join(metricScriptPath,file)
     metricScriptPathList.append(filepath)
-    print(filepath)
 
-slicer.util.loadScene(scenePath)
+# Load master scene from patient filepath
+for file in os.listdir(patientPath):
+    if file.endswith("-Scene.mrb"):
+        slicer.util.loadScene(os.path.join(patientPath,file))
 
 peNode = slicer.vtkMRMLPerkEvaluatorNode()
 logic = slicer.modules.perkevaluator.logic()
@@ -65,31 +69,43 @@ scopeTransform = slicer.mrmlScene.GetFirstNodeByName("ScopeToReference")
 # Set all needle roles to the ScopeToReference transform
 logic.SetMetricInstancesRolesToID(peNode,scopeTransform.GetID(),"Needle",slicer.vtkMRMLMetricInstanceNode.TransformRole)
 
-
 leftTransform = slicer.mrmlScene.GetFirstNodeByName("LeftHandToReference")
 logic.SetMetricInstancesRolesToID(peNode,leftTransform.GetID(),"LeftTool",slicer.vtkMRMLMetricInstanceNode.TransformRole)
-
 
 rightTransform = slicer.mrmlScene.GetFirstNodeByName("RightHandToReference")
 logic.SetMetricInstancesRolesToID(peNode,rightTransform.GetID(),"RightTool",slicer.vtkMRMLMetricInstanceNode.TransformRole)
 
-allSequenceBrowsers = slicer.mrmlScene.GetNodesByClass("vtkMRMLSequenceBrowserNode")
 
+allSequenceBrowsers = slicer.mrmlScene.GetNodesByClass("vtkMRMLSequenceBrowserNode")
 for browser in allSequenceBrowsers:
 
+    # Don't compute on empty sequences
     if browser.GetNumberOfItems() == 0:
         continue
     
+    # Don't compute on practice data if the user doesn't want to
     if browser.GetName() in practiceDataNameList and analyzePracticeData == False:
         continue
     
     # Set tracked sequence browser
     peNode.SetTrackedSequenceBrowserNodeID(browser.GetID())
     
+    sequenceName = browser.GetName()
+    
     # Set output metrics table
-    tableName = browser.GetName()+"_results"
+    tableName = sequenceName+"_results"
     resultTableNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLTableNode", tableName)
     peNode.SetMetricsTableID(resultTableNode.GetID())
     
     # Compute
     logic.ComputeMetrics(peNode)
+    
+
+    if saveResults == True:
+        # Make new folder if it doesn't exist
+        outputFolder = os.path.join(outputSavePath,patientID)
+        if not os.path.exists(outputFolder):
+            os.makedirs(outputFolder)
+        
+        # save node to csv file
+        slicer.util.saveNode(resultTableNode,os.path.join(outputFolder,sequenceName+".csv"))
