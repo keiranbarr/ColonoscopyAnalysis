2a68fa8dcf275cb82040f91a2128ebf16dae761e
Keiran Barr <keiranbarr7@gmail.com>
2022-07-20 16:32:23 -0400

    Analyze results working. Added roles class

--- AnalyzeResults.py
+++ AnalyzeResults.py
@@ -1,15 +1,36 @@
 import os
 import csv
 
-class Metric:
-
+# Each metric has typically 3 roles, but it varies
+# Hence the need for this
+class Role:
     def __init__(self,name):
         self.name = name
         self.values = []
-    
+        
     def AddValue(self,value):
         self.values.append(value)
 
+class Metric:
+    def __init__(self,name):
+        self.name = name
+        self.roles = []
+    
+    def AddRole(self,name):
+        self.roles.append(Role(name))
+        
+    def AddValueToRole(self,value,rolename):
+        for role in self.roles:
+            if role.name == rolename:
+                role.AddValue(value)
+                
+    def DoesRoleExist(self,rolename):
+        for role in self.roles:
+            if role.name == rolename:
+                return True
+        return False
+
+# User defined
 resultsPath = r'P:\data\PerkTutor\Colonoscopy\2016-2017-Protocol-Reorganized\Analysis\PerkTutorResults2022\CalculateMetricsOutput'
 metricScriptPath = r'C:\Users\Keiran Barr\Documents\Summer 22\ColonoscopyAnalysis\Colonoscopy-metrics'
 outputDataPath = r'P:\data\PerkTutor\Colonoscopy\2016-2017-Protocol-Reorganized\Analysis\PerkTutorResults2022\AnalyzeResultsOutput'
@@ -19,8 +40,7 @@ metricsObjectsList = []
 for metric in os.listdir(metricScriptPath):
     name = os.path.splitext(metric)[0]
     
-    newMetricObject = Metric(name)
-    metricsObjectsList.append(newMetricObject)
+    metricsObjectsList.append(Metric(name))
  
  
 for file in os.listdir(resultsPath):
@@ -37,15 +57,35 @@ for file in os.listdir(resultsPath):
                     metricUnit = row[2]
                     metricValue = row[3]
                         
+                    # Find corresponding metric object for the metric on this row
                     for metricObject in metricsObjectsList:
+                        
                         if metricObject.name == metricName.replace(" ",""):
-                            metricObject.AddValue(metricValue)
+                            
+                            if not metricObject.DoesRoleExist(metricRoles):
+                                metricObject.AddRole(metricRoles)
+                        
+                            metricObject.AddValueToRole(metricValue,metricRoles)
 
-                    
+
 with open(os.path.join(outputDataPath,"aggregateData.csv"), mode='w') as output_file:
     output_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     for metricObject in metricsObjectsList:
-        data = metricObject.values
-        data.insert(0,metricObject.name)
-        output_writer.writerow(data)
+        
+        roleKey = [metricObject.name+"_roles"]
+        metricValues = [metricObject.name+"_values"]
+
+        # Prevents saving results from metrics that weren't loaded correctly
+        saveMetricResults = False
+
+        for role in metricObject.roles:
+            saveMetricResults = True
+            for value in role.values:
+                
+                roleKey.append(role.name)
+                metricValues.append(value)
+          
+        if saveMetricResults:
+            output_writer.writerow(roleKey)
+            output_writer.writerow(metricValues)
